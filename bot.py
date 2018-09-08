908707cb1620b907c3b7f260382ae25b60dc9ca2
easaltykova <easaltykova@avito.ru>
2018-09-08 18:19:48 +0300

    add bot

---
+++ bot.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+
+def main():
+    mybot = Updater("659806032:AAEzxPcmOtNRtawBl_maB_zF4Nzxpz_oGFQ", request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    mybot.start_polling()
+    mybot.idle()
+
+
+def greet_user(bot, update):
+    text = 'Привет, человек!'
+    print(text)
+    update.message.reply_text(text)
+
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+
+main()
--- price.py
+++ price.py
@@ -2,3 +2,5 @@ def format_price(price):
 	return 'Цена: ' + str(int(price)) + ' руб.'
 
 print(format_price(56.24))
+
+
